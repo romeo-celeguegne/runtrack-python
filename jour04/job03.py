679bddffc675fc57e16738fc0ef95e8581677c1d
romeo-celeguegne <115151978+romeo-celeguegne@users.noreply.github.com>
2023-01-06 11:43:23 +0100

    PUSH JOUR04 RUNTRACK PYTHON

---
+++ jour04/job03.py
@@ -0,0 +1,7 @@
+def melon():
+    fruits = ["pomme", "cerise", "orange"]
+    fruits.append("Melon")
+    return fruits
+
+
+print(melon())
---
+++ jour04/job10.py
@@ -0,0 +1,7 @@
+L = [8, 24, 27, 48, 2, 16, 9, 102, 7, 84, 91]
+produit = 1
+for i in L:
+    if 25 <= i <= 90:
+        produit *= i
+
+print(f"Résultat produit [25 _ 90] = {produit}")
